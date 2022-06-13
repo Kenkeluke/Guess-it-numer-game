e6bcb268c034f9919e087b11ea1713edd68be816
Kenkeluke <88124618+Kenkeluke@users.noreply.github.com>
2022-06-13 05:19:02 +0100

    adding files

---
+++ level1.js
@@ -0,0 +1,14 @@
+let user = prompt("Enter your name to proceed");
+let greetingText;
+if (user == null){
+    alert("That's not a valid name, enter a valid name to continue");
+ }
+ else{
+     greetingText=`Welcome, ${user}`;
+ }
+const greetUser = () => {
+
+
+};
+
+console.log(greetingText);
